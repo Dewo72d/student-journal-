09d406d392ed04c15c670cad533fe5d5f9d44d9b
Dewo72d <diamsolo3228@gmail.com>
2021-03-29 22:01:11 +0300

    add-test-method-for-forms

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -2,14 +2,19 @@ const db = require("../database/database");
 //const jwt = require("jsonwebtoken");
 //const { jwtSecret } = require("../config/authConfig");
 
-exports.testlog = (req, res) => {
-  const testreq = req.body.testreq;
-  db.reqLog(testreq);
+exports.selection = (req, res) => {
+  const group = req.body.group;
+  const name = req.body.name;
+  const lesson = req.body.lesson;
+  const date = req.body.date;
+
+  console.log(group, name, lesson, date);
   res.sendStatus(200);
 };
 
 exports.test = (req, res) => {
-  let quer = "SELECT students.fullName,students.studentGroup,lesson.lessonNumber,lesson.value,lesson.Date FROM lesson,students WHERE YEAR(lesson.Date) = YEAR(CURRENT_DATE()) AND MONTH(lesson.Date) = MONTH(CURRENT_DATE()) AND lesson.studentId = students.id  ORDER BY Date ASC  "
+  let quer =
+    "SELECT students.fullName,students.studentGroup,lesson.lessonNumber,lesson.value,lesson.Date FROM lesson,students WHERE YEAR(lesson.Date) = YEAR(CURRENT_DATE()) AND MONTH(lesson.Date) = MONTH(CURRENT_DATE()) AND lesson.studentId = students.id  ORDER BY Date ASC  ";
   db.connection.query(quer, (err, result) => {
     if (err) console.log(err);
     console.log(result);
