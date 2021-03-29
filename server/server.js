3db0bd4a899e6e9c2c5298bfa5dd02e28f49dbb9
Dewo72d <diamsolo3228@gmail.com>
2021-03-29 22:00:12 +0300

    new-middleware

--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require("cors");
+const cors = require("cors");     // для кросдоменных запросов
+const multer = require("multer"); // для обработки react-hook-form или простых форм
+const upload = multer();
 const app = express();
 const port = 4000;
 
 app.use(cors());
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(upload.array());
+app.use(express.static("public"));
 
 app.use(require("./routes/router.js"));
 
