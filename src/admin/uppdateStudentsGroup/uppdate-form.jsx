641f1f980ec32865423d2cedc43b85ba3ce9d4ee
Vishnevskyi <76432370+Vishnevskyi@users.noreply.github.com>
2021-04-01 19:29:06 +0300

    Update uppdate-form.jsx

--- src/admin/uppdateStudentsGroup/uppdate-form.jsx
+++ src/admin/uppdateStudentsGroup/uppdate-form.jsx
@@ -1,119 +1,124 @@
-const db = require("../database/database");
-//const jwt = require("jsonwebtoken");
-//const { jwtSecret } = require("../config/authConfig");
-
-exports.selection = (req, res) => {
-  const group = req.body.group;
-  const name = req.body.name;
-  const lesson = req.body.lesson;
-  const date = req.body.date;
-
-  console.log(group, name, lesson, date);
-  let q = `SELECT students.fullName,students.studentGroup,lesson.lessonNumber,lesson.value FROM lesson,students WHERE lesson.lessonNumber=${lesson} `;
-  db.connection.query(q, (err, result) => {
-    if (err) console.log(err);
-    console.log(result);
-    res.send(result);
-  });
-};
-exports.insertingStudent = (req, res) => {
-  const name = req.body.name;
-  const group = req.body.group;
-  let q = `INSERT INTO students (studentGroup,fullName) VALUES ('${group}','${name}')`;//шукаємо в БД чи є такий студент
-  if (name === '' || group === '') {
-    res.status(200).json({
-      message: "Ви не ввели студента чи групу"
-    })
-  }
-  else {
-    let selectName = `SELECT * FROM students WHERE studentGroup = '${group}' AND fullName = '${name}'`;//шукаємо в БД чи є такий студент
-    db.connection.query(selectName, (err, result) => {
-      if (result.length > 0)
-        res.status(200).json({
-          message: "Такий студент вже існує"
-        })
-      else {
-        db.connection.query(q, (error, result2) => {//добавляємо в БД на основі вибірки
-          if (error) res.status(401).json({
-            message: "Помилка",
-          })
-          res.status(200).json({
-            message: "Студента було додано",
-          })
-        })
+import { Button, Collapse, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, IconButton, makeStyles } from "@material-ui/core"
+import { Alert } from "@material-ui/lab";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import CloseIcon from "@material-ui/icons/Close"
+const useStyles = makeStyles({
+    item:{
+        width: "100%",
+        margin: "0 auto"
+    },
+    alert: {
+        marginTop: "2em",
       }
-    });
-  }
-}
-exports.deletingStudent = (req, res) => {
-  const name = req.body.name;
-  const group = req.body.group;
-  console.log(group, name);
-  if (name.length == 0 || group.length == 0) {
-    res.status(200).json({
-      message: "Ви не ввели студента чи групу",
-    })
-  }
-  else {
-    let selectName = `SELECT * FROM students WHERE studentGroup = '${group}' AND fullName = '${name}'`;//шукаємо в БД чи є такий студент
-    db.connection.query(selectName, (err, result) => {
-      if (result.length == 0) {
-        res.status(200).json({
-          message: "Такого студента немає",
-        })
-      }
-      else {
-        let q = `DELETE FROM students WHERE studentGroup = '${group}' AND fullName = '${name}'`;//видаляємо на основі вибірки
-        db.connection.query(q, (error, result2) => {
-          if (error) res.status(401).json({
-            message: "Помилка",
-          })
-          res.status(200).json({
-            message: "Студента було видалено",
-          })
-        })
-      }
-    });
-  }
-}
-exports.uppdateStudent = (req, res) => {
-  let uppdateGroup = `UPDATE students SET students.studentGroup = students.studentGroup + 100 WHERE (students.studentGroup + 100) < 500`;
-  let selectGroup = `SELECT * FROM students WHERE (studentGroup + 100) > 500`;
-  let deleteStudentGroup = `DELETE FROM students WHERE (students.studentGroup + 100) > 500`;
-  db.connection.query(selectGroup, (err, result) => {//шукаємо випускників
-    if (result.length > 0)
-    {
-      db.connection.query(deleteStudentGroup,(err2,result2)=>{//видаляємо їх
-        if (err2) 
-        {
-          console.log(err2);
-          res.status(404).json({
-            message:"Помилка"
-          })
+})
+function UppdateForm() {
+    const {register, handleSubmit} = useForm();
+    const [result, setResult] = useState([]);
+    const [open,setOpen] = useState(false);
+    const [alertopen, setAlertOpen] = useState(true);
+    const handleOpen = () => {
+      setOpen(true);
+    };
+    const handleClose = () => {
+      setOpen(false);
+      setAlertOpen(true);
+    };
+    const onSubmit = async (data) => {
+        let formData = new FormData();
+        for (let key in data) {
+          formData.append(key, data[key]);
         }
-        else
-        {
-          db.connection.query(uppdateGroup,(err3,result3)=>{//переводимо інших на наступний курс
-           if (err) res.status(404).json({
-             message:"Помилка"
-           })
-           else{
-            res.status(200).json({
-              message:"Переведення успішно здійснено"
-            })
-           }
+        await fetch("http://localhost:4000/api/uppdatestudents", {
+          method: "POST",
+          mode: "cors",
+          body: formData,
+        })
+          .then(async (res) => {
+            return await await res.json();
           })
-        }
-      })
+          .then(async (res) => {
+             return await setResult(res);
+          })
+          .catch((err) => {
+            console.log(err);
+          }, []);
+      };
+    const onErr = (err) =>{
+        console.log(err);
     }
-  })
+    const classes = useStyles();
+    return (
+        <div>
+            <form onSubmit={handleSubmit(onSubmit,onErr)} id="myform3">
+            <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"Продовжити?"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Ви впевнені,що хочете перевести студентів на наступний курс?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Закрити
+          </Button>
+          <Button variant="contained" color="primary" type="submit" form="myform3" onClick={handleClose}>
+            Далі
+        </Button>
+        </DialogActions>
+      </Dialog>
+      <Button className={classes.item} variant="contained" color="primary" onClick={handleOpen}>
+          Відправити
+      </Button>
+      <div className={classes.alert}>
+          {result.message === 'Помилка' ? (<div>
+      <Collapse in={alertopen}>
+        <Alert
+         severity="warning"
+          action={
+            <IconButton
+              aria-label="close" 
+              color="inherit"
+              size="small"
+              onClick={() => {
+                setAlertOpen(false);
+              }}
+            >
+              <CloseIcon fontSize="inherit" />
+            </IconButton>
+          }
+        >
+          <strong>{result.message}</strong>
+        </Alert>
+      </Collapse>
+    </div>): result.length == 0 ?(<div></div>):(<div>
+      <Collapse in={alertopen}>
+        <Alert
+          action={
+            <IconButton
+              aria-label="close" 
+              color="inherit"
+              size="small"
+              onClick={() => {
+                setAlertOpen(false);
+              }}
+            >
+              <CloseIcon fontSize="inherit" />
+            </IconButton>
+          }
+        >
+          <strong>{result.message}</strong>
+        </Alert>
+      </Collapse>
+    </div>)}
+          </div>
+            </form>
+        </div>
+    )
 }
-exports.test = (req, res) => {
-  let quer =
-    "SELECT students.fullName,students.studentGroup,lesson.lessonNumber,lesson.value,lesson.Date FROM lesson,students WHERE YEAR(lesson.Date) = YEAR(CURRENT_DATE()) AND MONTH(lesson.Date) = MONTH(CURRENT_DATE()) AND lesson.studentId = students.id  ORDER BY Date ASC  ";
-  db.connection.query(quer, (err, result) => {
-    if (err) console.log(err);
-    console.log(result);
-    res.send(result);
-  });
-};
+export default UppdateForm;
