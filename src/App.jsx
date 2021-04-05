163b8125ea0d4102abf3c84ccad13720c23e427d
Dewo72d <diamsolo3228@gmail.com>
2021-04-05 08:33:53 +0300

    nothing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import admin from "./admin/admin-page";
-import { BrowserRouter, Route } from "react-router-dom";
+import {BrowserRouter, Route} from "react-router-dom";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Route path="/mypage" component={admin} />
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Route path="/mypage" component={admin}/>
+        </BrowserRouter>
+    );
 }
 
 export default App;
