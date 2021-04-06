217d81171eff88d332e5168c6f18cb1060786d4d
Dewo72d <diamsolo3228@gmail.com>
2021-04-06 22:28:11 +0300

    nothing

--- src/admin/admin-page.jsx
+++ src/admin/admin-page.jsx
@@ -1,46 +1,51 @@
 import React from "react";
 import FilterForm from "./filter-form/filter-form";
+
 import Paper from "@material-ui/core/Paper";
-import { Accordion, AccordionDetails, AccordionSummary, makeStyles, Typography } from "@material-ui/core";
+import {Accordion, AccordionDetails, AccordionSummary, makeStyles, Typography} from "@material-ui/core";
+
 const useStyles = makeStyles({
-  card:{
-    display:"block"
-}
+    card: {
+        display: "block"
+    }
 })
+
 function AdminPage() {
-  const classes = useStyles();
-  return (
-    <Paper>
-      <Accordion>
-        <AccordionSummary>
-          <Typography>Пошук студентів</Typography>
-        </AccordionSummary>
-        <AccordionDetails className={classes.card}>
-          <FilterForm />
-        </AccordionDetails>
-      </Accordion>
-      <Accordion>
-        <AccordionSummary>
-          <Typography>Вставка студентів</Typography>
-        </AccordionSummary>
-        <AccordionDetails className={classes.card}>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion>
-        <AccordionSummary>
-          <Typography>Перевід на новий курс</Typography>
-        </AccordionSummary>
-        <AccordionDetails className={classes.card}>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion>
-        <AccordionSummary>
-          <Typography>Видалення студентів</Typography>
-        </AccordionSummary>
-        <AccordionDetails className={classes.card}>
-        </AccordionDetails>
-      </Accordion>
-    </Paper>
-  );
+    const classes = useStyles();
+    return (
+        <Paper>
+            <Accordion>
+                <AccordionSummary>
+                    <Typography>Пошук студентів</Typography>
+                </AccordionSummary>
+                <AccordionDetails className={classes.card}>
+                    <FilterForm/>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion>
+                <AccordionSummary>
+                    <Typography>Вставка студентів</Typography>
+                </AccordionSummary>
+                <AccordionDetails className={classes.card}>
+
+                </AccordionDetails>
+            </Accordion>
+            <Accordion>
+                <AccordionSummary>
+                    <Typography>Перевід на новий курс</Typography>
+                </AccordionSummary>
+                <AccordionDetails className={classes.card}>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion>
+                <AccordionSummary>
+                    <Typography>Видалення студентів</Typography>
+                </AccordionSummary>
+                <AccordionDetails className={classes.card}>
+                </AccordionDetails>
+            </Accordion>
+        </Paper>
+    );
 }
+
 export default AdminPage;
