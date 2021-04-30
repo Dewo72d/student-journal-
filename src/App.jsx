d2d649959acd469444451154e8570a42818fa905
Dewo72d <66914271+Dewo72d@users.noreply.github.com>
2021-04-30 08:47:44 +0300

    add prepod page

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import admin from "./admin/admin-page";
 import loginPage from "./login/login-page/login-page";
 import starosta from "./starosta/starosta-page";
 import manager from "./manager/manager-page";
+import Teacher from "./prepod/prepod-page";
 import {BrowserRouter, Route, Redirect} from "react-router-dom";
 
 function App() {
     let [autorized, setAutorized] = useState({status: "Authorized"});
     let [path, setPath] = useState({path: "/auth"});
+    let [groupPrepod, setGroupPrepod] = useState(0);
 
     useEffect(() => {
         fetch("http://localhost:4000/api/cookie", {
@@ -16,10 +18,11 @@ function App() {
             credentials: "include",
             body: document.cookie,
         }).then(async (res) => {
-            const role = Object.values(await res.json())[1];
-            console.log(Object.values(await res), Object.keys(await res));
+            const usersData = Object.values(await res.json());
+            const role = await usersData[1];
+            setGroupPrepod(await usersData[0]);
             await setAutorized({status: res.statusText}); //Установка значения ЗАЛОГИНИЛСЯ/НЕ ЗАЛОГИНИЛСЯ
-            await setPath(role === "admin" ? {path: "/mypage"} : role === "starosta" ? {path: "/starosta"} : role === "manager" ? {path: "/manager"} : {path: "/auth"}); //ПРОВЕРКА И ВЫДАЧА СТРАНИЧКИ
+            await setPath(role === "admin" ? {path: "/mypage"} : role === "starosta" ? {path: "/starosta"} : role === "manager" ? {path: "/manager"} : role === "prepod" ? {path: "/teacher"} : {path: "/auth"}); //ПРОВЕРКА И ВЫДАЧА СТРАНИЧКИ
         });
     }, []);
 
@@ -34,7 +37,7 @@ function App() {
     return (
         <BrowserRouter>
             <Redirect push to={path.path}/>
-            <Route exact path="/auth" component={loginPage}/>
+            <Route render={()=> <Teacher groupPrepod={groupPrepod} />} path="/teacher" />
             <Route path="/starosta" component={starosta}/>
             <Route path="/mypage" component={admin}/>
             <Route path="/manager" component={manager}/>
